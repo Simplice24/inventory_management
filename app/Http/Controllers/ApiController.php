c03c4d67630e983583331f22097cd36ae2dfdd58
Simplice24 <nsimplice0@gmail.com>
2024-01-09 20:16:27 +0200

    APIs

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class ApiController extends Controller
+{
+    public function users()
+    {
+        $users = User::all(); 
+        return response()->json($users); 
+    }
+
+    public function user($id)
+    {
+        $user = User::find($id);
+        return response()->json($user);
+    }
+
+    public function userUpdate(Request $request, $id)
+    {
+        // Validate the request data
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $id,
+            'password' => 'sometimes|string|min:8' 
+        ]);
+
+        // Find the user by ID
+        $user = User::findOrFail($id);
+
+        // Update the user's information
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        // Update the password only if it's provided
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->save();
+
+        return response()->json(['message' => 'User updated successfully!', 'user' => $user]);
+    }
+
+    public function deleteUser($id){
+        $user = User::find($id);
+        $user->delete();
+        return response()->json(['message' => 'User is deleted successfully']);
+    }
+}
