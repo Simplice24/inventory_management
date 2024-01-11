cc464517961446b218b874fb6989e2b12179f23b
Simplice24 <nsimplice0@gmail.com>
2024-01-11 07:47:53 +0200

    Dashboard metrics accuracy

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ProductionMaterial;
+use App\Models\RawMaterial;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
   public function index()
   {
-    return view('dashboard');
+    // Calculate the sum of 'material_quantity'
+    $totalMaterialQuantity = ProductionMaterial::sum('material_quantity');
+    $totalRawMaterialQuantity = RawMaterial::sum('material_quantity');
+    $totalUsers = User::count();
+
+    // Pass the total to the 'dashboard' view
+    return view('dashboard', ['totalMaterialQuantity' => $totalMaterialQuantity,'totalRawMaterialQuantity' => $totalRawMaterialQuantity, 'totalUsers' => $totalUsers]);
   }
 }
