3e441d14d059b59bd6b316659c30765d3f70d995
Simplice24 <nsimplice0@gmail.com>
2024-01-11 08:12:18 +0200

    Dashboard metrics accuracy

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,11 +20,22 @@ class DashboardController extends Controller
     
     $rawMatReqCount = RawMatReqConfirmation::where('status', 0)->count();
     $productionMatReqCount = ProductionMatReqConfirmation::where('status', 0)->count();
-
     $totalPendingRequest = $rawMatReqCount + $productionMatReqCount;
+
+    $rawMatReqCountConfirmed = RawMatReqConfirmation::where('status', 1)->count();
+    $productionMatReqCountConfirmed = ProductionMatReqConfirmation::where('status', 1)->count();
+    $totalConfirmed = $rawMatReqCountConfirmed + $productionMatReqCountConfirmed;
+
+    $rawMatReqCountCancelled = RawMatReqConfirmation::where('status', 2)->count();
+    $productionMatReqCountCancelled = ProductionMatReqConfirmation::where('status', 2)->count();
+    $totalCancelled = $rawMatReqCountCancelled + $productionMatReqCountCancelled;
+
+
+    
 
     // Pass the total to the 'dashboard' view
     return view('dashboard', ['totalMaterialQuantity' => $totalMaterialQuantity,'totalRawMaterialQuantity' => $totalRawMaterialQuantity,
-     'totalUsers' => $totalUsers, 'totalPendingRequest' => $totalPendingRequest]);
+     'totalUsers' => $totalUsers, 'totalPendingRequest' => $totalPendingRequest, 'totalCancelled' => $totalCancelled,
+     'totalConfirmed' => $totalConfirmed]);
   }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -96,6 +96,40 @@
           </div>
         </div>
       </div>
+      <!-- Confirmed Requests Card Example -->
+      <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-warning shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                  Confirmed Requests</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalConfirmed }}</div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-comments fa-2x text-gray-300"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Cancelled Requests Card Example -->
+      <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-warning shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                  Cancelled Requests</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalCancelled }}</div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-comments fa-2x text-gray-300"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 @endsection
